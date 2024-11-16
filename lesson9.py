9b35deb6aaea94e2bb72ce20636582da4fbf6c16
student <student@itstep1.dp.ua>
2024-11-16 17:25:14 +0200

    Lesson9

--- lesson9.py
+++ lesson9.py
@@ -1,4 +1,31 @@
-print("Lesson 9!")
+print('lesson 9: parsing of site')
 import requests
+from bs4 import BeautifulSoup
+import sqlite3
 
-data_site = requests.get(https://coinmarketcap.com/) #Посилання на сайт
+
+data_site = requests.get('https://coinmarketcap.com/')
+print('site coin market status code', data_site.status_code)
+
+rates = []
+if data_site.status_code == 200:
+    print('coin market is connected')
+    soup = BeautifulSoup(data_site.text, features="html.parser")
+    soup_list = soup.find_all('div', {'class': 'sc-b3fc6b7-0 dzgUIj'})
+
+    for block in soup_list:
+        # print('parse', block)
+        rate = float(block.findNext().text[1:].replace(',', ''))
+        rates.append(rate)
+
+print('value rates: ', rates)
+
+sl3_connection = sqlite3.connect('crypto_ratesDB.sl3', 5)
+sl3_cur =sl3_connection.cursor()
+
+print('\nsl3 connection', sl3_connection)
+print('cur', sl3_cur)
+
+sl3_cur.execute('CREATE TABLE crypto_rates (print_date DATETIME, rate FLOAT)')
+
+sl3_connection.close()
